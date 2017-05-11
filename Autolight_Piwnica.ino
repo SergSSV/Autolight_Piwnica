b5424cbc81c4fe4dd6fbf06e5a5faadfee3d3fcd
adik1323 <adik1323@gmail.com>
2017-05-11 22:20:51 +0200

    changes in main loop and sensor reading

--- Autolight_Piwnica.ino
+++ Autolight_Piwnica.ino
@@ -9,9 +9,9 @@
 #define IR_led 13             //IR indicator led
 
 bool debug = 0;        //debug mode on/off (serial port)
-bool R_move2, PIR_move, active, IR_obstackle, wake_up;
+bool Radar_move, PIR_move, ACTIVE, IR_obstackle, wake_up;
 unsigned long movements_counter;
-unsigned long timer, soft_hold = 10000, wake_hold = 5500, long_timer = 300000; //10s, 5,5s, 5min time
+unsigned long timer, soft_hold = 10000, wake_hold = 6000, long_timer = 300000; //10s, 6s, 5min time
 byte trigger1 = 155, trigger2 = 160;   // IR sensors triggering distance
 
 void setup()
@@ -26,50 +26,39 @@ void setup()
 }
 
 void loop()
-{ IR_sensor_check();      //turns active to 1 if IR senses something
+{ IR_sensor_check();            // IR distance sensors check door - entrance
+  Movement_Sensors_Check();     // handles PIR and Radar sensor - check movement inside the room
+  
+  if (ACTIVE)
+  { digitalWrite(LED, HIGH);            //main LED light ON
     
-  while (active)
-  { digitalWrite(LED, HIGH);            //main LED light ON (2nd time for sure)
-    timer = millis();
-    
-    while (movements_counter <= 1 && !wake_up)      // <= x numbers of radar sensings
-    { Movement_Sensors_Check();
-      if (R_move2 || PIR_move)                      //if flag coming from any sensor
+    if (movements_counter <= 1 && !wake_up)         // <= x numbers of movement sensings
+    { if (Radar_move || PIR_move)                   //if flag coming from any sensor
         timer = millis();                           //loop timeout refresh
-      IR_sensor_check();                            //check IR sensor, if it senses something then break this loop
-      if (timer + soft_hold < millis())             //after no movement for soft_hold time - break from loop
-        { wake_up = 1;                              //go next to wake_up loop
+      if (timer + soft_hold < millis())             //after no movement for soft_hold time - go to wake-up()
+        { wake_up = 1;
           timer = millis();                         //refresh timer for wake_up loop
-          break;
         }
       if(debug) Serial.println(movements_counter);
     }
     
-    while (movements_counter >= 2 && !wake_up)      // >= x numbers of radar sensings
-    { Movement_Sensors_Check();
-      if (R_move2 || PIR_move)                      //if flag coming from any sensor
+    if (movements_counter >= 2 && !wake_up)         // >= x numbers of movement sensings
+    { if (Radar_move || PIR_move)                   //if flag coming from any sensor
         timer = millis();                           //loop timeout refresh
-      IR_sensor_check();                            //check IR sensor, if it senses something then break this loop
-      if (timer + long_timer < millis())            //after 5 min break from loop and check movement
-        { movements_counter = 0;
-          break;
-        }
+      if (timer + long_timer < millis())            //after 5 min no mevement - go to soft_hold
+        movements_counter = 0;
       if(debug) Serial.println(movements_counter);
     }
 
-    while (wake_up)                          //called after every timeout, lasts for few sec.
-    { digitalWrite(LED, LOW);                //main LED OFF
-      IR_sensor_check();                     //check IR sensor, if it senses something then break this loop
-      Movement_Sensors_Check();
-      if (PIR_move)                          //if any movement happens - go back to work
+    if (wake_up)                                    //called after every timeout, lasts for few sec.
+    { digitalWrite(LED, LOW);                       //main LED OFF
+      if (PIR_move)                                 //if PIR senses movement - go back to work
         { digitalWrite(LED, HIGH);
-          wake_up = 0;                       //reset "wake_up" flag to break from this loop
-          movements_counter++;
+          wake_up = 0;                              //reset "wake_up" flag to break from this condition
+          movements_counter++;                      //go stright to 2nd condition - "long_timer"
         }
-      if (timer + wake_hold < millis())      //if the loop time expires, turn all OFF and wait for IR
-        { digitalWrite(LED, LOW);
-          Variable_Reset();
-        }
+      if (timer + wake_hold < millis())             //if the loop time expires - reset all variables and wait for IR
+        Variable_Reset();
       if(debug) Serial.println(movements_counter);
     }
   }
--- sensors.ino
+++ sensors.ino
@@ -19,12 +19,13 @@ void IR_sensor_check()
     }
   }
   if (distance1 >= trigger1 || distance2 >= trigger2)  
-  { active = 1;
+  { ACTIVE = 1;
+    timer = millis();
     movements_counter = 0;
-    wake_up = 0;
+    wake_up = 0;    
+    Radar_move = 0;
     digitalWrite(IR_led, HIGH);      //shows sensor work
-    R_move2 = 0;
-    analogWrite(radar_led, 0);    // radar reset
+    analogWrite(radar_led, 0);       // radar reset
   }
   else
     digitalWrite(IR_led, LOW);     
@@ -37,44 +38,41 @@ void IR_sensor_check()
 void Variable_Reset()
 { movements_counter = 0;
   wake_up = 0;
-  active = 0;
+  ACTIVE = 0;
   PIR_move = 0;
-  R_move2 = 0;
+  Radar_move = 0;
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////
 void Movement_Sensors_Check() 
-{ int analog3 = analogRead(PIR);                                    // PIR
-  if (analog3 > 300 && !PIR_move)
+{ if (analogRead(PIR) > 300 && !PIR_move)
     { movements_counter ++;
       PIR_move = 1;
       analogWrite(pir_led, 100);
     }
-  if (analog3 < 300)
+  if (analogRead(PIR) < 300)
     { PIR_move = 0;
       analogWrite(pir_led, 0);
     }
-  if(debug) { Serial.print("PIR: "); Serial.println(analog3); }
-///////////////
+  if(debug) { Serial.print("PIR: "); Serial.println(analogRead(PIR)); }
+///////////////////////////
   if (movements_counter > 0)
-  { int analog2 = analogRead(radar);                                 // radar 2
-    if (analog2 > 300 && !R_move2)
+  { if (analogRead(radar) > 300 && !Radar_move)
       { movements_counter++;
-        R_move2 = 1;
+        Radar_move = 1;
         analogWrite(radar_led, 100);
       }
-    if (analog2 < 300)
-      { R_move2 = 0;
+    if (analogRead(radar) < 300)
+      { Radar_move = 0;
         analogWrite(radar_led, 0);
       }
-    if(debug) { Serial.print("radar: "); Serial.println(analog2); }
+    if(debug) { Serial.print("radar: "); Serial.println(analogRead(radar)); }
   }
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////
 void Switch_check()
-{ int button = analogRead(manual_switch);
-  while(button < 200)
+{ while(analogRead(manual_switch) < 200)
   { digitalWrite(LED, HIGH);            //main LED light ON
     if(debug) 
       Serial.println("Driver OFFLINE");
@@ -85,15 +83,15 @@ void Switch_check()
 //////////////////////////////////////////////////////////////////////////////////////////////
 void PowerUP()
 { analogWrite(pir_led, 100);
-  delay(200);
+  delay(100);
   analogWrite(radar_led, 100);
-  delay(200);
+  delay(100);
   digitalWrite(IR_led, HIGH);
-  delay(200);
+  delay(100);
   analogWrite(pir_led, 0);
-  delay(200);
+  delay(100);
   analogWrite(radar_led, 0);
-  delay(200);
+  delay(100);
   digitalWrite(IR_led, LOW);
 }
 
